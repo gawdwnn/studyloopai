764437db6d7c26b01edcd2510259f1e0f31e2947
godwin <ooguejiofor4787@conestogac.on.ca>
2025-05-28 02:43:34 -0400

    Update project configuration and add Shadcn UI components
    - Enhanced biome.json with additional linter rules and JavaScript formatter settings.
    - Updated package.json to include new dependencies and scripts for testing and linting.
    - Modified globals.css to change font settings and add custom animations.
    - Refactored layout.tsx to integrate ThemeProvider and updated font imports.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -25,9 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
